212b1067c60c2208d71a89a0a3a82283d4389301
Bruno Alexandre <bruno.alexandre@mfmti.com.br>
2023-02-10 15:52:36 -0300

    Criar entidades de dominio e contratos da API

---
+++ HyperativaDesafioTecnico/src/Cartao.Domain/Entities/Cartao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperativaDesafio.Domain.Entities
+{
+    internal class Cartao
+    {
+        public int id { get; set; }
+        public string numeroHash { get; set; }
+        public string numeroMascara { get; set; }
+        public string numeracaoNoLote { get; set; } 
+        public DateTime dataCadastro { get; set; }
+        public Lote lote { get; set; }
+    }
+}
---
+++ HyperativaDesafioTecnico/src/Cartao.Domain/Entities/Lote.cs
@@ -0,0 +1,16 @@
+﻿namespace HyperativaDesafio.Domain.Entities
+{
+    public class Lote
+    {
+
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string data { get; set; }
+        public string lote { get; set; }
+        public string qtdeRegistros { get;set; }
+        public string header { get; set; }
+        public string trailer { get; set; }
+        public string dataProcessamento { get; set; }
+        public string tipoLote { get; set; }
+    }
+}

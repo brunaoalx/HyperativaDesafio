49bd365c6bf9c5ae3d45996daa9a3353b8e3806f
Bruno Alexandre <bruno.alexandre@mfmti.com.br>
2023-02-11 09:50:08 -0300

    Iniciar a Classe de conexao com banco.

---
+++ HyperativaDesafioTecnico/src/Infra.Data/Context/HyperativaDesafioDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Dapper;
+
+namespace HyperativaDesafio.Infra.Data.Context
+{
+    public class HyperativaDesafioDbContext
+    {
+        public SqliteConnection Connection { get; private set; }
+        public HyperativaDesafioDbContext(string connectionString)
+        {
+            SetConnection(connectionString);
+        }
+
+        private void SetConnection(string connectionString)
+        {
+            try
+            {
+                this.Connection = new SqliteConnection(connectionString);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
